7705b61c1ce1d6cf651f84738d9bb696a1e2c18d
appgp05 <alberto.puig.parellada@gmail.com>
2025-10-07 09:41:56 +0200

    .

--- Desarrollo en Cliente/Ejercicios/Ejercicios UD2/op2.js
+++ Desarrollo en Cliente/Ejercicios/Ejercicios UD2/op2.js
@@ -5,6 +5,26 @@
 
 // alert(mensaje)
 
+// EJERCICIO 2
+function enviarFormularioEj2(){
+    let ej2Numero1 = document.getElementById("ej2Numero1").value
+    let ej2Numero2 = document.getElementById("ej2Numero2").value
+
+    let ej2Resultado = ej2Numero1 >> Math.sqrt(ej2Numero2)
+
+    document.getElementById("resultadosEj2").innerHTML = ej2Resultado
+}
+
+// EJERCICIO 3
+function enviarFormularioEj3(){
+    let ej3Numero1 = document.getElementById("ej3Numero1").value
+    let ej3Numero2 = document.getElementById("ej3Numero2").value
+
+    let ej3Resultado = ej3Numero1 << Math.sqrt(ej3Numero2)
+
+    document.getElementById("resultadosEj3").innerHTML = ej3Resultado
+}
+
 // EJERCICIO 4
 console.log("El número más cercano al 0 es " + Number.MIN_VALUE)
 console.log("El número más alto posible es " + Number.MAX_VALUE)
