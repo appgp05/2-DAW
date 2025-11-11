90e72da980f350d46966a43fa904377138a19bc5
appgp05 <alberto.puig.parellada@gmail.com>
2025-11-11 09:08:56 +0100

    .

--- Desarrollo en Cliente/UD3/Ejercicio/ejercicios_ampliacion/Ejercicio 1/js/tabla.js
+++ Desarrollo en Cliente/UD3/Ejercicio/ejercicios_ampliacion/Ejercicio 1/js/tabla.js
@@ -5,16 +5,58 @@ let peon = "♙"; // VARIBALE DE PEONES
 /* CREAMOS EL TABLERO */
 onload = function tablero() {
  /* REFERIMOS LA TABLA VACÍA EN UNA VARIABLE */
- 
+ let tabla = document.getElementById("tablero")
 
  /* CREA EN LA TABLA 8 FILAS CON DO WHILE */
- 
+let i = 0
+let filas = []
+do {
+    filas.push(tabla.insertRow(i))
+    
+    for(let j = 0; j < 8; ++j){
+        let columna
+        switch (i) {
+            case 0:        
+                columna = filas[i].insertCell()
+                columna.innerHTML = piezas[j]
+                columna.classList.add("negras")
+                break;
+            case 1:
+                columna = filas[i].insertCell()
+                columna.innerHTML = peon
+                columna.classList.add("negras")
+                break;
+            case 6:
+                columna = filas[i].insertCell()
+                columna.innerHTML = peon
+                columna.classList.add("blancas")
+                break;
+            case 7:        
+                columna = filas[i].insertCell()
+                columna.innerHTML = piezas[j]
+                columna.classList.add("blancas")
+                break;
+            default:
+                columna = filas[i].insertCell()
+                break;
+        }
+    }
+    
+    ++i
+} while (i < 8)
 
  /* INSERTA CADA FILA DEL TABLERO EN UNA NUEVA VARIABLE FILA: .insertRow(0), .insertRow(1)....*/
   
-  
- /* CREA EN CADA FILA 8 CELDAS. EMPLEA FOR  */
+//  console.log(tabla.insertRow(1))
 
+//  filas[1].innerHTML = "a"
+//  console.log(filas)
+
+
+/* CREA EN CADA FILA 8 CELDAS. EMPLEA FOR  */
+for(let i = 0; i < 8; ++i){
+
+}
 
  
  /* INSERTA TODAS LAS CELDAS EN SU CORRESPONDIENTES 8 FILAS (a modo de columnas) .insertCell(0), .insertCell(0)*/
@@ -27,6 +69,8 @@ onload = function tablero() {
  
 
 
+ console.log(tabla)
 
 }
 
+//PRACTICAR CON UN FORMULARIO HECHO CON JS Y LUEGO VALIDAR LOS INSERTS
