60c29a9f24d469f4765b6b184831061b98452247
appgp05 <alberto.puig.parellada@gmail.com>
2025-11-11 09:48:48 +0100

    .

--- Desarrollo en Cliente/UD3/Ejercicio/ejercicios_ampliacion/Ejercicio 1/js/tabla.js
+++ Desarrollo en Cliente/UD3/Ejercicio/ejercicios_ampliacion/Ejercicio 1/js/tabla.js
@@ -5,57 +5,61 @@ let peon = "♙"; // VARIBALE DE PEONES
 /* CREAMOS EL TABLERO */
 onload = function tablero() {
  /* REFERIMOS LA TABLA VACÍA EN UNA VARIABLE */
- let tabla = document.getElementById("tablero")
+let tabla = document.getElementById("tablero")
 
  /* CREA EN LA TABLA 8 FILAS CON DO WHILE */
 let i = 0
-let filas = []
 do {
-    filas.push(tabla.insertRow(i))
-    
-    for(let j = 0; j < 8; ++j){
-        let columna
-        switch (i) {
-            case 0:        
-                columna = filas[i].insertCell()
-                columna.innerHTML = piezas[j]
-                columna.classList.add("negras")
-                break;
-            case 1:
-                columna = filas[i].insertCell()
-                columna.innerHTML = peon
-                columna.classList.add("negras")
-                break;
-            case 6:
-                columna = filas[i].insertCell()
-                columna.innerHTML = peon
-                columna.classList.add("blancas")
-                break;
-            case 7:        
-                columna = filas[i].insertCell()
-                columna.innerHTML = piezas[j]
-                columna.classList.add("blancas")
-                break;
-            default:
-                columna = filas[i].insertCell()
-                break;
-        }
-    }
-    
+    tabla.insertRow(i)    
     ++i
 } while (i < 8)
 
+// let i = 0
+// let filas = []
+// do {
+//     filas.push(tabla.insertRow(i))
+    
+//     for(let j = 0; j < 8; ++j){
+//         let columna
+//         switch (i) {
+//             case 0:        
+//                 columna = filas[i].insertCell()
+//                 columna.innerHTML = "<div class='negras'>"+piezas[j]+"</div>"
+//                 break;
+//             case 1:
+//                 columna = filas[i].insertCell()
+//                 columna.innerHTML = "<div class='negras'>"+peon+"</div>"
+//                 break;
+//             case 6:
+//                 columna = filas[i].insertCell()
+//                 columna.innerHTML = "<div class='blancas'>"+peon+"</div>"
+//                 break;
+//             case 7:        
+//                 columna = filas[i].insertCell()
+//                 columna.innerHTML = "<div class='blancas'>"+piezas[j]+"</div>"
+//                 break;
+//             default:
+//                 columna = filas[i].insertCell()
+//                 break;
+//         }
+//     }
+    
+//     ++i
+// } while (i < 8)
+
  /* INSERTA CADA FILA DEL TABLERO EN UNA NUEVA VARIABLE FILA: .insertRow(0), .insertRow(1)....*/
   
-//  console.log(tabla.insertRow(1))
+let filas = []
 
-//  filas[1].innerHTML = "a"
-//  console.log(filas)
-
+for(let row of tabla.rows){
+    filas.push(row)
+}
 
 /* CREA EN CADA FILA 8 CELDAS. EMPLEA FOR  */
-for(let i = 0; i < 8; ++i){
-
+for(let i = 0; i < filas.length; ++i){
+    for(let j = 0; j < 8; ++j){
+        filas[i].insertCell(j)
+    }
 }
 
  
@@ -66,6 +70,26 @@ for(let i = 0; i < 8; ++i){
 
  /* AHORA QUE TENEMOS EL TABLERO CONSTRUIDO DE 8X8; CON INNERHTML Y EMPLEADO SWITCH-CASE RELLENA LAS FILAS 0 Y 7 CON LAS PIEZAS CORRESPONDIENTES Y LAS FILAS 1 Y 6 CON LOS PEONES*/
 
+for(let i = 0; i < filas.length; ++i){
+    for(let j = 0; j < 8; ++j){
+        switch (i) {
+            case 0:            
+                tabla.rows[i].cells[j].innerHTML = "<div class='negras'>"+piezas[j]+"</div>"
+                break;
+            case 1:
+                tabla.rows[i].cells[j].innerHTML = "<div class='negras'>"+peon+"</div>"
+                break;
+            case 6:
+                tabla.rows[i].cells[j].innerHTML = "<div class='blancas'>"+peon+"</div>"
+                break;
+            case 7:        
+                tabla.rows[i].cells[j].innerHTML = "<div class='blancas'>"+piezas[j]+"</div>"
+                break;
+            default:
+                break;
+        }
+    }
+}
  
 
 
