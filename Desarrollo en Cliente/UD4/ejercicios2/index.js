9cef7965c9a8e8d7e1956660c8483a30a03d56aa
appgp05 <alberto.puig.parellada@gmail.com>
2025-11-25 08:57:20 +0100

    .

--- Desarrollo en Cliente/UD4/ejercicios2/index.js
+++ Desarrollo en Cliente/UD4/ejercicios2/index.js
@@ -14,3 +14,48 @@ let a = (...argumentos) => {
 }
 
 a(1, 2, 3, 4, 5, 6, 7, 8)
+
+const CONTRASENA = ""
+
+let contrasenaValidada = false
+
+let validar = () => {
+    let contrasena = document.getElementById("contrasena").value;
+
+    let sesion = document.getElementById("sesion")
+
+    let requierenSesion = document.getElementsByClassName("requiere-sesion")
+
+    if(contrasena == CONTRASENA){
+        contrasenaValidada = true
+        sesion.style.backgroundColor = "green"
+        sesion.innerHTML = "<p>Sesión iniciada</p>"
+        for(requiereSesion of requierenSesion){
+            requiereSesion.disabled = false
+        }
+    } else {
+        contrasenaValidada = false
+        sesion.style.backgroundColor = "red"
+        sesion.innerHTML = "<p>Contrasña incorrecta</p>"
+        for(requiereSesion of requierenSesion){
+            requiereSesion.disabled = true
+        }
+    }
+}
+
+let saldo = 10000
+
+let consultar = () => {
+    let htmlResultadOperacion = document.getElementById("resultadoOperacion")
+    htmlResultadOperacion.innerHTML = "Tu saldo es de "+saldo+"€"
+}
+
+let sacar = () => {
+    let cantidad = document.getElementById("montoSacar").value
+
+    saldo = saldo - cantidad
+    
+    let htmlResultadOperacion = document.getElementById("resultadoOperacion")
+    htmlResultadOperacion.innerHTML = "Tu saldo es de "+saldo+"€"
+
+}
