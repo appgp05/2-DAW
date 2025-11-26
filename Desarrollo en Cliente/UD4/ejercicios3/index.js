1eca34f5f6ec759776caff0d307b65f30fb7ba5e
appgp05 <alberto.puig.parellada@gmail.com>
2025-11-26 11:31:35 +0100

    .

--- Desarrollo en Cliente/UD4/ejercicios3/index.js
+++ Desarrollo en Cliente/UD4/ejercicios3/index.js
@@ -21,6 +21,18 @@ let construir = (...argumentos) => {
     return personas
 }
 
+let calcularMediaEdadPersonas = (personas) => {
+    let resultado = 0
+
+    for(let persona of personas){
+        resultado += Number(persona.edad)
+    }
+
+    resultado = resultado/personas.length
+
+    return resultado
+}
+
 let personas = construir(
     ["Roberto", "Martínez", "22", "roberto.martinez@tuempresa.com"],
     ["Antonio", "López", "25", "antonio.lopez@tuempresa.com"],
@@ -28,8 +40,11 @@ let personas = construir(
     ["Eva", "Teruel", "33", "eva.teruel@tuempresa.com"],
 )
 
+let edadMediaPersonas = calcularMediaEdadPersonas(personas)
+
 // let persona1 = new Persona("Roberto", "Martínez", "22", "roberto.martinez@tuempresa.com")
 // let persona2 = new Persona("Antonio", "López", "25", "antonio.lopez@tuempresa.com")
 // let persona3 = new Persona("Javier", "Rodríguez", "18", "javier.rodriguez@tuempresa.com")
 // let persona4 = new Persona("Eva", "Teruel", "33", "eva.teruel@tuempresa.com")
-console.log(personas)
+console.log("Personas creadas: ", ...personas)
+console.log("La edad media de las personas es: ", edadMediaPersonas)
