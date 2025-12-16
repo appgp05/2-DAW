2151b3152856d74f6bd70cd77a1baf985b0e8534
appgp05 <alberto.puig.parellada@gmail.com>
2025-12-16 13:17:00 +0100

    .

--- DIseño de interfaces/Actividad 04_03/script.js
+++ DIseño de interfaces/Actividad 04_03/script.js
@@ -1,16 +1,29 @@
 $().ready(() => {
-    $("video").addClass("oculto")
+    $("#video").addClass("oculto")
 
     $("#botonMostrarOcultarReproductor").on("click", () => {
-        $("video").removeClass("oculto")
+        if($("#video").hasClass("oculto")){
+            $("#video").removeClass("oculto")
+        } else {
+            $("#video").addClass("oculto")
+        }
+    })
+
+    $("#botonComprobarPausa").on("click", ()=>{
+        if($("#video").get(0).paused){
+            console.log("El viídeo está pausado")
+        } else {
+            console.log("El vídeo se está reproduciendo")
+        }
     })
 
     $("#botonReproducir").on("click", ()=>{
-        $("video").get(0).play()
+        $("#video").get(0).play()
     })
 
     $("#botonPausar").on("click", ()=>{
-        $("video").get(0).pause()
-        console.log($("video").get(0).paused)
+        $("#video").get(0).pause()
     })
+
+    $("#indicadoresVideo").text("asdasdads");
 })
