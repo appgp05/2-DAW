9a1a228553e674a73bee222bcdb507e231a2db31
appgp05 <alberto.puig.parellada@gmail.com>
2025-12-17 10:10:13 +0100

    .

--- DIseño de interfaces/Actividad 04_03/script.js
+++ DIseño de interfaces/Actividad 04_03/script.js
@@ -1,5 +1,30 @@
 $().ready(() => {
+    const player = new Plyr('#video', {
+        controls: [
+            "play",
+            "progress",
+            "current-time",
+            "mute",
+            "volume",
+            "fullscreen"
+        ]
+    });
+
     $("#video").addClass("oculto")
+
+    $("div > video").each(function () {
+        this.on("click", function () {
+            this.addClass("resaltado")
+        })
+    })
+
+    $("#checkboxAceptoReproduccion").on("change", function ()  {
+        const checked = this.checked
+
+        $(".requiereAceptarReproduccion").each(function () {
+            this.disabled = !checked
+        })
+    })
 
     $("#botonMostrarOcultarReproductor").on("click", () => {
         if($("#video").hasClass("oculto")){
@@ -17,13 +42,33 @@ $().ready(() => {
         }
     })
 
+    // $(".requiereAceptarReproduccion").prop("disabled", true)
+
+    $(".requiereAceptarReproduccion").each(function () {
+        this.disabled = true
+    })
+
     $("#botonReproducir").on("click", ()=>{
-        $("#video").get(0).play()
+        if($("#checkboxAceptoReproduccion").get(0).checked){
+            $("#video").get(0).play()
+        } else {
+            alert("Debes aceptar reproducir el contenido multimedia mediante el checkbox")
+        }
     })
 
     $("#botonPausar").on("click", ()=>{
         $("#video").get(0).pause()
     })
 
-    $("#indicadoresVideo").text("asdasdads");
+
+    setInterval(() => {
+        const video = $("#video").get(0)
+
+        const tiempoActual = video.currentTime.toFixed()
+        const tiempoTotal = video.duration.toFixed()
+        const porcentaje = (tiempoActual * 100 / tiempoTotal).toFixed()
+
+
+        $("#indicadoresVideo").text(tiempoActual + ":" + tiempoTotal + " | " + porcentaje + "%");
+    }, 1000)
 })
