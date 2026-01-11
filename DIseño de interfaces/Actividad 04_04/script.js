29df13f629883f4cb84e790d5bdb1e1e74137b0f
appgp05 <alberto.puig.parellada@gmail.com>
2026-01-11 14:15:58 +0100

    .

--- DIseño de interfaces/Actividad 04_04/script.js
+++ DIseño de interfaces/Actividad 04_04/script.js
@@ -9,4 +9,52 @@ $().ready(() => {
     $("nav > *").each(function () {
         console.log(this)
     })
+
+    let lis = $("#contenedor").find("li")
+    console.log(lis)
+    
+    let textos = $("#form").find("input[type=text]")
+    console.log(textos)
+
+    let articles = $("section").find("article")
+    console.log(articles)
+    
+    console.log(articles)
+    
+    $("section").each(function () {
+        let articles2 = $(this).find("article")
+
+        console.log(articles2 ," son los artículos de",$(this))
+    })
+
+    $("#buttonResaltarFinds").on("click", () => resaltarFinds([lis, textos, articles]))
+
+    let parent = $($("li").get(0)).parent()
+    console.log(parent)
+
+    let articleSubido = $($("article").get(0)).parent()
+    let sectionSubido = articleSubido.parent().parent()
+    console.log(articleSubido)
+    console.log(sectionSubido)
+
+    let divContenedor = $("#contenedor")
+    let hijosDivContenedor = divContenedor.children()
+    console.log(divContenedor)
+    console.log(hijosDivContenedor)
 })
+
+let resaltado = false
+
+function resaltarFinds(finds){
+    for(let find of finds){
+        $(find).each(function() {
+            if(resaltado){
+                $(this).removeClass("resaltado")
+            } else {
+                $(this).addClass("resaltado")
+            }
+        })
+    }
+    
+    resaltado = !resaltado
+}
