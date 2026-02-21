df61387aee2a6caa1ba9c8c0391ae0e2f0d9fc23
appgp05 <alberto.puig.parellada@gmail.com>
2026-02-21 18:27:04 +0100

    .

--- Desarrollo en Cliente/2 Trimestre/Tema 5/index.js
+++ Desarrollo en Cliente/2 Trimestre/Tema 5/index.js
@@ -1,15 +1,3 @@
-function mostrarEnConsola(texto){
-    console.log(texto)
-}
-
-addEventListener("DOMContentLoaded", () => {
-    document.getElementById("botonConManejadorSemántico").onclick = () => mostrarEnConsola("Con manejador semántico")
-    document.getElementById("botonConAddEventListener").addEventListener("click", () => {
-        mostrarEnConsola("Con AddEventListener")
-    })
-    document.getElementById("botonConSetAtribute").setAttribute("onclick", "mostrarEnConsola('Con SetAtribute')")
-})
-
 // PRUEBAS CLASE
 
 addEventListener("DOMContentLoaded", () => {
